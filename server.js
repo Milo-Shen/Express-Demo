12b3f47932ad9fc11c6e7e66c538df79d18d5d5b
Milo-Shen <Milo-Shen@outlook.com>
2024-02-19 15:07:03 +0800

    feat: add /api/login

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('./models');
 
 const express = require('express');
-const { User } = require('./models');
+const bcrypt = require('bcrypt');
 
+const { User } = require('./models');
 const app = express();
 app.use(express.json());
 
@@ -25,6 +26,32 @@ app.post('/api/register', async (request, response) => {
   }
 
   response.send(user);
+});
+
+app.post('/api/login', async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({
+    username: username,
+  });
+
+  if (!user) {
+    response.status(422).send({
+      message: 'user not found',
+    });
+  }
+
+  const is_password_valid = bcrypt.compareSync(password, user.password);
+
+  if (!is_password_valid) {
+    return response.status(422).send({
+      message: 'password is invalid',
+    });
+  }
+
+  response.send({
+    token: 'fake token',
+    message: user,
+  });
 });
 
 app.get('/api/users', async (request, response) => {
